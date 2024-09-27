fa62e300fe32f40df6edbf599271b745db16ed34
SUBHAMAG30 <119666254+SUBHAMAG30@users.noreply.github.com>
2024-09-27 18:00:06 +0000

    do

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,13 @@
+const accountId = 85412
+let accountEmail = "agarwalsubham0011@gmail.com"
+var accountPsswrd = "122334"
+accountCity = "Jaipur"
+let accountState
+
+// accountId = 2
+accountEmail = "abc@.com"
+accountPsswrd = "7826294"
+accountCity = "Kolkata"
+
+console.log(accountId)
+console.table([accountId,accountEmail,accountPsswrd,accountCity,accountState])
