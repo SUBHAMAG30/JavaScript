5260112384d4cec90045e502016ee79357514e49
SUBHAMAG30 <119666254+SUBHAMAG30@users.noreply.github.com>
2024-09-27 18:10:25 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,12 @@
+"use strict"; // treat all JS code as newer version
+
+//alert( 3 + 3) --- we cannot use this in nodejs, not browser
+
+console.log(3+3);
+console.log("subham");
+
+
+console.log(typeof undefined);//undefined
+console.log(typeof null);//object
+
+
