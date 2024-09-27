5472f46862f5979e60a275fcdad83ff924352bfa
SUBHAMAG30 <119666254+SUBHAMAG30@users.noreply.github.com>
2024-09-27 18:49:07 +0000

    Conversions of data types

---
+++ 01_basics/03_Conversion.js
@@ -0,0 +1,26 @@
+let score = "Subham"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "23" => 23
+// "45sjg" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1=> true; 0=>false
+// "" => false
+// "anything" => true
+
+let someNumber = 98;
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
