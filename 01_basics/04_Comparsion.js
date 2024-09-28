40c24fc1eb26e965fd00b79d0db065742a0693e6
SUBHAMAG30 <119666254+SUBHAMAG30@users.noreply.github.com>
2024-09-28 05:54:22 +0000

    Comparisons between operators

---
+++ 01_basics/04_Comparsion.js
@@ -0,0 +1,20 @@
+// 6 other types of relational opertors....we know them....
+
+console.log("2" > 1);
+console.log("02" > 1);
+console.log("02" > "1");
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === => strict check
+console.log("2" == 2); // Data type diff but no issue
+
+console.log("2" === 2);//strictly checks, so big issue
+
+
