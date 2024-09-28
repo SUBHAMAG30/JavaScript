f23fbea5237f957d2d773759435de49d61d0b061
SUBHAMAG30 <119666254+SUBHAMAG30@users.noreply.github.com>
2024-09-28 06:08:14 +0000

    Data Types

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -10,3 +10,28 @@ console.log(typeof undefined);//undefined
 console.log(typeof null);//object
 
 
+// Primitive
+// 7 types: String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.21
+
+const Id = Symbol('123')
+const AnotherId = Symbol('123')
+console.log(Id === AnotherId);
+
+const bigNumber = 6793764873743289748n
+
+
+// Reference (Non Primitive)
+// Arrays, Objects, Functions
+
+const heros = ["shaktiman", "nagagraj", "doga"];
+let myobj = {
+    name: "Subham",
+    age: 21,
+}
+
+const myFun = function(){
+    console.log("HEllo");
+}
