3cfd4c7b0e5a6918a8059fa5992f9b3b7a4d3016
SUBHAMAG30 <119666254+SUBHAMAG30@users.noreply.github.com>
2024-09-28 05:41:21 +0000

    conversion

--- 01_basics/03_Conversion.js
+++ 01_basics/03_Conversion.js
@@ -22,5 +22,28 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 98;
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+let value = 7
+let negValue = -value
+console.log(negValue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2*2);
+// console.log(2%2);
+
+
+let str1 = "HEllo"
+let str2 = " Subham"
+
+let str3 = str1 +str2
+console.log(str3);
+
+console.log("1" + 3 + 2);
+console.log(1 + 3 + "2");
