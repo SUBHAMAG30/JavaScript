a4982737940eaae4b1f0eec47513d0f7b79e2edd
SUBHAMAG30 <119666254+SUBHAMAG30@users.noreply.github.com>
2024-10-04 19:04:31 +0000

    if else

---
+++ 02_basics_DS/06_controlFlow.js
@@ -0,0 +1,47 @@
+// // if
+// const isUserloggedIn = true
+// const temperature = 41
+
+// if ( temperature <= 50 ){
+//     console.log("less than 50");
+// } else {
+//     console.log("temperature is greater than 50");
+// }
+
+// console.log("Execute");
+// <, >, <=, >=, ==, !=, ===, !==
+
+// const score = 200
+
+// if (score > 100) {
+//     let power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+
+// console.log(`User power: ${power}`);
+
+
+// const balance = 1000
+
+// if(balance > 500) console.log("test"),
+// console.log("test2");
+
+// if(balance < 500) {
+//     console.log("less than");
+// } else if(balance < 750)
+//     console.log("Less than 750");
+// else
+//     console.log("less than 1000");
+    
+const UserLoggedIn = true
+const debitCard = true
+const loggedInG = false
+const loggedInE = true
+if(UserLoggedIn && debitCard && 2!=3)
+{
+    console.log("Allow to buy products");
+}
+
+if(loggedInE || loggedInG)
+    console.log("User Logged In");
+    
